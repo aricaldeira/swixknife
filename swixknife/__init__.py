9feae99e0bbb577d8631da465744d0cf2784432c
Ari Caldeira <ari.caldeira@tauga.com.br>
2023-05-07 18:55:10 -0300

    [FEA] Importing everything

--- swixknife/__init__.py
+++ swixknife/__init__.py
@@ -1,5 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# SwixKnife - Sezimal (base six) in Python
+#
+# Copyright (C) 2023–
+# Copyright (C) Ari Caldeira <aricaldeira at gmail.com>
+#
+#
+
+__author__ = 'Ari Caldeira'
+__copyright__ = 'Copyright 2023, SwixKnife'
+__credits__ = ['Ari Caldeira', 'Irv Bromberg']
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__status__ = 'Beta'
+
 
 from .base import *
-from .sezimal import Sezimal, SezimalInteger, SezimalFraction
+from .sezimal import *
 from .functions import *
 from .exponents import *
+from .units import *
+from .date_time import *
