ea7337c7920120bef46ab37342d0915fcdfe13d2
Ari Caldeira <ari.caldeira@tauga.com.br>
2023-05-07 18:55:36 -0300

    [FEA] Building wheel

---
+++ clean_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm dist -rf
+rm build -rf
+rm swixknife.egg-info -rf
+
+python setup.py clean
+python setup.py bdist_wheel
+cp dist/*.whl clock/static/python/
+
+rm swixknife.egg-info -rf
+# rm build -rf
+#rm dist -rf
---
+++ setup.py
@@ -0,0 +1,11 @@
+
+
+from setuptools import setup, find_packages
+import swixknife
+
+
+setup(
+    name = 'swixknife',
+    version = swixknife.__version__,
+    packages = find_packages(),
+)
