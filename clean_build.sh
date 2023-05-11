365d4e3f095c3c4a147fd5076c18bc91ca294114
Ari Caldeira <ari.caldeira@tauga.com.br>
2023-05-11 13:21:32 -0300

    [IMP] Change build wheel to docs

--- clean_build.sh
+++ clean_build.sh
@@ -6,7 +6,7 @@ rm swixknife.egg-info -rf
 
 python setup.py clean
 python setup.py bdist_wheel
-cp dist/*.whl clock/static/python/
+cp dist/*.whl docs/static/python/
 
 rm swixknife.egg-info -rf
 # rm build -rf
