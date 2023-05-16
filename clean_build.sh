cb3b097a4cbbdcdf3db3223d1a328a7171205b13
Ari Caldeira <ari.caldeira@tauga.com.br>
2023-05-16 17:37:41 -0300

    [IMP] Better cleaning pre-compiled code

--- clean_build.sh
+++ clean_build.sh
@@ -8,6 +8,13 @@ python setup.py clean
 python setup.py bdist_wheel
 cp dist/*.whl docs/static/python/
 
+#
+# Clean compilation
+#
+find . -type f -name "*.py[co]" -delete
+find . -type d -name "__pycache__" -delete
+find . -type f -name "*.py~" -delete
 rm swixknife.egg-info -rf
+
 # rm build -rf
 #rm dist -rf
