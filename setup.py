c3f2025352a409ea800eef00a5ca32cc971a7c6b
Ari Caldeira <ari.caldeira@tauga.com.br>
2023-08-02 15:54:40 -0300

    [WIP] Cythonize on install

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 
 
 from setuptools import setup, find_packages
+# from Cython.Build import cythonize
 import swixknife
 
+packages = find_packages()
+modules = [pack.replace('.', '/') + '/*.py' for pack in packages]
 
 setup(
-    name = 'swixknife',
-    version = swixknife.__version__,
-    packages = find_packages(),
-    package_data={'': ['*.sor']}
+    name='swixknife',
+    version=swixknife.__version__,
+    packages=packages,
+    package_data={
+        '': [
+            '*.sor',
+            'sun_moon.db',
+        ]
+    },
+    # ext_modules=cythonize(
+    #     module_list=modules,
+    #     language_level='3',
+    # )
 )
