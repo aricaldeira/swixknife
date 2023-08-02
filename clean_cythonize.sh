080a0eb1e074333d20a6e182efe321a5a8726369
Ari Caldeira <ari.caldeira@tauga.com.br>
2023-08-02 19:56:11 -0300

    [FIX] Typo

--- clean_cythonize.sh
+++ clean_cythonize.sh
@@ -10,7 +10,7 @@ rm build -rf
 python setup.py build_ext
 
 mkdir dist
-cp build/lib.linux-x86_64-cpython-311/swixknife -R dist/
+cp build/lib.linux-x86_64-cpython-311/swixknife/swixknife -R dist/
 cp swixknife/text/data/*.sor dist/swixknife/text/data/
 cp swixknife/date_time/sun_moon.db dist/swixknife/date_time/
 rm build -rf
