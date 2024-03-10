876ce9ddf2cac31c4dccd010d9d885fa382a7eb1
Ari Caldeira <ari.caldeira@tauga.com.br>
2024-03-09 22:06:41 -0300

    [FEA] Adjusting fractions to contextual controls

--- swixknife/sezimal.py
+++ swixknife/sezimal.py
@@ -1132,8 +1132,11 @@ class SezimalFraction(Sezimal):
 
         self._numerator = Sezimal(cleaned_numerator)
         self._denominator = Sezimal(cleaned_denominator)
-        self._sezimal = self._numerator / self._denominator
-        # self._sezimal = Sezimal(self._numerator.decimal / self._denominator.decimal)
+
+        if sezimal_context.fractions_use_decimal:
+            self._sezimal = Sezimal(self._numerator.decimal / self._denominator.decimal)
+        else:
+            self._sezimal = self._numerator / self._denominator
 
         super().__init__(self._sezimal)
 
@@ -1280,21 +1283,33 @@ class SezimalFraction(Sezimal):
 
         return super().__pow__(other_number)
 
+    @property
+    def decimal_fraction(self):
+        return DecimalFraction(*self.as_decimal_integer_ratio())
+
     def simplify(self) -> FractionSelf:
-        num, den = self.__simplify(self.numerator, self.denominator)
+        num, den = self.__simplify(self.numerator, self.denominator, force=True)
 
         if num == self.numerator and den == self.denominator:
             return self
 
         return SezimalFraction(num, den)
 
-    def __simplify(self, num, den):
+    def __simplify(self, num, den, force: bool = False):
+        if (not sezimal_context.fractions_simplify) and (not force):
+            return num, den
+
         num = num.decimal
         den = den.decimal
 
-        _PRIMES = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223)
+        # df = DecimalFraction(int(num), int(den))
+        #
+        # num = Decimal(df.numerator)
+        # den = Decimal(df.denominator)
+        #
+        # return num, den
 
-        for factor in _PRIMES:
+        for factor in _sezimal_maps._PRIMES:
             factor = Decimal(factor)
 
             if factor > min(num, den):
@@ -1315,10 +1330,6 @@ class SezimalFraction(Sezimal):
                 den = den_test
 
         return SezimalInteger(num), SezimalInteger(den)
-
-    # @property
-    # def decimal(self):
-    #     return DecimalFraction(*self.as_decimal_integer_ratio())
 
 
 _numbers.Number.register(Sezimal)
