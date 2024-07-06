d919dbeb992ed7ab444afb4f845bd38f48b051e3
Ari Caldeira <ari.caldeira@tauga.com.br>
2024-07-06 19:52:22 -0300

    [IMP] Allowing use greek letter constants as valid number input

--- swixknife/base/validation.py
+++ swixknife/base/validation.py
@@ -137,13 +137,13 @@ _FRACTION_SEPARATOR = '\\.'
 _RECURRING_MARKER = re.compile('\\.\\.')
 _CLOSING_RECURRING_MARKER = re.compile('\\.\\.\\.$')
 
-_VALID_SEZIMAL_FORMAT = re.compile(r'''^[+-]?[0-5]{1,}\.{0,2}([Ee][+-]?[0-5]{1,})?$|^[+-]?[0-5]*\.[0-5]{1,}([Ee][+-]?[0-5]{1,})?$|^[+-]?[0-5]*\.\.[0-5]{1,}(\.\.\.)?$|^[+-]?[0-5]*\.[0-5]{1,}\.\.[0-5]{1,}(\.\.\.)?$''')
+_VALID_SEZIMAL_FORMAT = re.compile(r'''^[+-]?[0-5]{1,}\.{0,2}([Ee][+-]?[0-5]{1,})?$|^[+-]?[0-5]*\.[0-5]{1,}([Ee][+-]?[0-5]{1,})?$|^[+-]?[0-5]*\.\.[0-5]{1,}(\.\.\.)?$|^[+-]?[0-5]*\.[0-5]{1,}\.\.[0-5]{1,}(\.\.\.)?|[ΦφΠπΤτ]$''')
 
-_VALID_DECIMAL_FORMAT = re.compile(r'''^[+-]?[0-9]{1,}\.{0,2}([Ee][+-]?[0-9]{1,})?$|^[+-]?[0-9]*\.[0-9]{1,}([Ee][+-]?[0-9]{1,})?$|^[+-]?[0-9]*\.\.[0-9]{1,}(\.\.\.)?$|^[+-]?[0-9]*\.[0-9]{1,}\.\.[0-9]{1,}(\.\.\.)?$''')
+_VALID_DECIMAL_FORMAT = re.compile(r'''^[+-]?[0-9]{1,}\.{0,2}([Ee][+-]?[0-9]{1,})?$|^[+-]?[0-9]*\.[0-9]{1,}([Ee][+-]?[0-9]{1,})?$|^[+-]?[0-9]*\.\.[0-9]{1,}(\.\.\.)?$|^[+-]?[0-9]*\.[0-9]{1,}\.\.[0-9]{1,}(\.\.\.)?|[ΦφΠπΤτ]$''')
 
-_VALID_NIFTIMAL_FORMAT = re.compile(r'''^[+-]?[0-9A-Za-z]{1,}\.{0,2}([Ee][+-][0-9A-Za-z]{1,})?$|^[+-]?[0-9A-Za-z]*\.[0-9A-Za-z]{1,}([Ee][+-][0-9A-Za-z]{1,})?$|^[+-]?[0-9A-Za-z]*\.\.[0-9A-Za-z]{1,}(\.\.\.)?$|^[+-]?[0-9A-Za-z]*\.[0-9A-Za-z]{1,}\.\.[0-9A-Za-z]{1,}(\.\.\.)?$''')
+_VALID_NIFTIMAL_FORMAT = re.compile(r'''^[+-]?[0-9A-Za-z]{1,}\.{0,2}([Ee][+-][0-9A-Za-z]{1,})?$|^[+-]?[0-9A-Za-z]*\.[0-9A-Za-z]{1,}([Ee][+-][0-9A-Za-z]{1,})?$|^[+-]?[0-9A-Za-z]*\.\.[0-9A-Za-z]{1,}(\.\.\.)?$|^[+-]?[0-9A-Za-z]*\.[0-9A-Za-z]{1,}\.\.[0-9A-Za-z]{1,}(\.\.\.)?|[ΦφΠπΤτ]$''')
 
-_VALID_DOZENAL_FORMAT = re.compile(r'''^[+-]?[0-9↊↋ABab]{1,}\.{0,2}([Ee][+-]?[0-9↊↋ABab]{1,})?$|^[+-]?[0-9↊↋ABab]*\.[0-9↊↋ABab]{1,}([Ee][+-]?[0-9↊↋ABab]{1,})?$|^[+-]?[0-9↊↋ABab]*\.\.[0-9↊↋ABab]{1,}(\.\.\.)?$|^[+-]?[0-9↊↋ABab]*\.[0-9↊↋ABab]{1,}\.\.[0-9↊↋ABab]{1,}(\.\.\.)?$''')
+_VALID_DOZENAL_FORMAT = re.compile(r'''^[+-]?[0-9↊↋ABab]{1,}\.{0,2}([Ee][+-]?[0-9↊↋ABab]{1,})?$|^[+-]?[0-9↊↋ABab]*\.[0-9↊↋ABab]{1,}([Ee][+-]?[0-9↊↋ABab]{1,})?$|^[+-]?[0-9↊↋ABab]*\.\.[0-9↊↋ABab]{1,}(\.\.\.)?$|^[+-]?[0-9↊↋ABab]*\.[0-9↊↋ABab]{1,}\.\.[0-9↊↋ABab]{1,}(\.\.\.)?|[ΦφΠπΤτ]$''')
 
 
 def validate_clean_sezimal(number: int | float | str | Decimal | Sezimal | SezimalInteger | SezimalFraction, double_precision: bool = False) -> str:
@@ -152,7 +152,8 @@ def validate_clean_sezimal(number: int | float | str | Decimal | Sezimal | Sezim
 
     number = str(number)
 
-    cleaned_number = number.translate(_CLEAN_SPACES)
+    cleaned_number = number.translate(_SEZIMAL_CONSTANTS)
+    cleaned_number = cleaned_number.translate(_CLEAN_SPACES)
     cleaned_number = sezimal_to_default_digits(cleaned_number)
 
     if not _VALID_SEZIMAL_FORMAT.match(cleaned_number):
@@ -189,7 +190,8 @@ def validate_clean_sezimal(number: int | float | str | Decimal | Sezimal | Sezim
 def validate_clean_decimal(number: int | float | str | Decimal) -> str:
     number = str(number)
 
-    cleaned_number = number.translate(_CLEAN_SPACES)
+    cleaned_number = number.translate(_DECIMAL_CONSTANTS)
+    cleaned_number = cleaned_number.translate(_CLEAN_SPACES)
 
     if not _VALID_DECIMAL_FORMAT.match(cleaned_number):
         raise ValueError(f'The number {number} has an invalid format for a decimal number')
@@ -222,7 +224,8 @@ def validate_clean_decimal(number: int | float | str | Decimal) -> str:
 def validate_clean_niftimal(number: int | float | str | Decimal | Sezimal) -> str:
     number = str(number)
 
-    cleaned_number = number.translate(_CLEAN_SPACES).upper()
+    cleaned_number = number.translate(_NIFTIMAL_CONSTANTS)
+    cleaned_number = cleaned_number.translate(_CLEAN_SPACES).upper()
     cleaned_number = dedicated_niftimal_to_default_digits(cleaned_number)
 
     if not _VALID_NIFTIMAL_FORMAT.match(cleaned_number):
@@ -256,7 +259,8 @@ def validate_clean_niftimal(number: int | float | str | Decimal | Sezimal) -> st
 def validate_clean_dozenal(number: str) -> str:
     number = str(number)
 
-    cleaned_number = number.translate(_CLEAN_SPACES).upper()
+    cleaned_number = number.translate(_DOZENAL_CONSTANTS)
+    cleaned_number = cleaned_number.translate(_CLEAN_SPACES).upper()
 
     if not _VALID_DOZENAL_FORMAT.match(cleaned_number):
         raise ValueError(f'The number {number} has an invalid format for a dozenal number')
@@ -410,6 +414,55 @@ def _exponent_to_full_form(number: str, base: int = 6) -> str:
     return number
 
 
+_SEZIMAL_CONSTANTS = {
+    ord('Φ'):
+        '1.341_254_554_353_431_451_342_235_140_150_120_045_250_124_044_114',
+    ord('φ'):
+        '1.341_254_554_353_431_451_342_235_140_150_120_045_250_124_044_114',
+    ord('Π'):
+        '3.050_330_051_415_124_105_234_414_053_125_321_102_301_214_442_004',
+    ord('π'):
+        '3.050_330_051_415_124_105_234_414_053_125_321_102_301_214_442_004',
+    ord('Τ'):
+        '10.141_100_143_234_252_214_513_232_150_255_042_205_002_433_324_012',
+    ord('τ'):
+        '10.141_100_143_234_252_214_513_232_150_255_042_205_002_433_324_012',
+}
+
+_DECIMAL_CONSTANTS = {
+    ord('Φ'):
+        '1.618_033_988_749_894_848_204_586_834_365_638_118',
+    ord('φ'):
+        '1.618_033_988_749_894_848_204_586_834_365_638_118',
+    ord('Π'):
+        '3.141_592_653_589_793_238_462_643_383_279_502_884',
+    ord('π'):
+        '3.141_592_653_589_793_238_462_643_383_279_502_884',
+    ord('Τ'):
+        '6.283_185_307_179_586_476_925_286_766_559_005_768',
+    ord('τ'):
+        '6.283_185_307_179_586_476_925_286_766_559_005_768',
+}
+
+_NIFTIMAL_CONSTANTS = {
+    ord('Φ'): '1.M8YZRXRAVMENA1U80TH1G4PA',
+    ord('φ'): '1.M8YZRXRAVMENA1U80TH1G4PA',
+    ord('Π'): '3.53I5AB8P5FSA5JHK72I8ASC4',
+    ord('π'): '3.53I5AB8P5FSA5JHK72I8ASC4',
+    ord('Τ'): '6.A70AKMHEAVKKB2Z4E50GLKO8',
+    ord('τ'): '6.A70AKMHEAVKKB2Z4E50GLKO8',
+}
+
+_DOZENAL_CONSTANTS = {
+    ord('Φ'): '1.74↋↋6772802↊46↊6↊186530714908↊0912714↊',
+    ord('φ'): '1.74↋↋6772802↊46↊6↊186530714908↊0912714↊',
+    ord('Π'): '3.184809493↋918664573↊6211↋↋151551↊04708',
+    ord('π'): '3.184809493↋918664573↊6211↋↋151551↊04708',
+    ord('Τ'): '6.349416967↋635108↋2790423↋↊2↊2↊↊3809926',
+    ord('τ'): '6.349416967↋635108↋2790423↋↊2↊2↊↊3809926',
+}
+
+
 def _make_validation_expression(all_digits='[0-5]') -> re.Pattern:
     fraction_separator = _FRACTION_SEPARATOR
     recurring_marker = f'{_RECURRING_MARKER.pattern}'
@@ -434,7 +487,9 @@ def _make_validation_expression(all_digits='[0-5]') -> re.Pattern:
     else:
         exponent = f'([Ee][+-]?{required_digits})?'
 
-    all_pattern = f'^{integer}{exponent}$|^{common_fractional}{exponent}$|^{recurring_fractional}$|^{complex_recurring_fraction}$'
+    constants = '[ΦφΠπΤτ]'
+
+    all_pattern = f'^{integer}{exponent}$|^{common_fractional}{exponent}$|^{recurring_fractional}$|^{complex_recurring_fraction}|{constants}$'
 
     return re.compile(all_pattern)
 
