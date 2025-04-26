55cb97192ac12a682788645c777a75700970585d
Ari Caldeira <ari.caldeira@tauga.com.br>
2025-04-26 13:41:14 -0300

    [IMP] Improving time zones

--- time_zones/compile_install.sh
+++ time_zones/compile_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sudo rm /usr/share/zoneinfo/SPM
-sudo rm /usr/share/zoneinfo/NT4
-sudo rm /usr/share/zoneinfo/NT6
+sudo rm /usr/share/zoneinfo/GPM -rf
+sudo rm /usr/share/zoneinfo/SPM -rf
+sudo rm /usr/share/zoneinfo/NT4 -rf
+sudo rm /usr/share/zoneinfo/NT6 -rf
 sudo rm /usr/share/zoneinfo/Sezimal -rf
 sudo rm /usr/share/zoneinfo/Dozenal -rf
 sudo rm /usr/share/zoneinfo/Natural -rf
 
-sudo zic spm_and_natural_time_zones.txt
+sudo zic gpm_time_zones.txt
+sudo zic spm_time_zones.txt
+sudo zic sezimal_time_zones.txt
+sudo zic dozenal_time_zones.txt
