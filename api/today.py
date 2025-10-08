ad3c029a23c37929c79efb5f433a12fa974863ac
Ari Caldeira <ari.caldeira@tauga.com.br>
2025-10-08 20:40:12 -0300

    [IMP] Slightly bigger display

--- api/today.py
+++ api/today.py
@@ -2700,9 +2700,9 @@ def adc_circle(hemisphere: str = 'S') -> Response:
     return Response(text, mimetype='image/svg+xml')
 
 
-if True:
+def _preload_calendars():
     for year_diff in (0,):  # (0, 1, -1):
-        for locale_code in ('pt-br', 'bz-br', 'eo-br', 'en-br', 'en-us', 'en-gb', 'en-ca', 'en-in', 'en-au'):
+        for locale_code in ('pt-br', 'bz-br', 'eo-br', 'en-br'):  #, 'en-us', 'en-gb', 'en-ca', 'en-in', 'en-au'):
             sym_year = SezimalDate.today().year + year_diff
             dcc_year = SezimalDate.today().dcc_year + year_diff
             iso_year = SezimalDate.today().gregorian_date.year + year_diff
@@ -2768,3 +2768,5 @@ if True:
                             'format_token': '',
                         },
                     )
+
+_preload_calendars()
